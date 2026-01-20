b7471a1c4dfc70b384a7775ab6f0018ba93533de
40438752 <jbrown101@qub.ac.uk>
2026-01-20 13:35:36 +0000

    UI Implementation of Roads + Ports.

---
+++ src/main/java/com/example/viewmodel/RoadViewState.java
@@ -0,0 +1,9 @@
+package com.example.viewmodel;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public final class RoadViewState {
+    public final IntegerProperty owner = new SimpleIntegerProperty(-1);
+}
+
