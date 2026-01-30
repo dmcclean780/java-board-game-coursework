d7d001b8e84c8483cd410b74235910ba2b8c5798
40452739 <elarner01@qub.ac.uk>
2026-01-30 14:12:42 +0000

    Local fonts issues fix

--- src/main/java/com/example/service/NavigationService.java
+++ src/main/java/com/example/service/NavigationService.java
@@ -1,13 +1,13 @@
 package com.example.service;
 
+import java.io.IOException;
+
+import com.example.view.ViewModelAware;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
-import com.example.view.ViewModelAware;
 
 public class NavigationService {
 
